220012561dd81bc3202047c1f281f818445f18f0
Bjørn André Svellingen Lavik <bjornandre.s.lavik@hotmail.com>
2019-10-30 20:33:50 +0100

    Initial commit

---
+++ Python/Assignment_1.py
@@ -0,0 +1,15 @@
+def pi(d):
+    short = math.pi
+    short = d.length
+    return short
+
+def main():
+    input("Type in a number")
+    number = pi(d)
+    print(number)
+
+
+
+
+if __name__ == "__main__":
+    main()
